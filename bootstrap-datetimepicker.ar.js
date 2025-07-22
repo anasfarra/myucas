b3f150325843c8cce51c8add945e1d0fd6076048
anasfarra <124315266+anasfarra@users.noreply.github.com>
2025-07-23 00:58:41 +0300

    Add files via upload

---
+++ bootstrap-datetimepicker.ar.js
@@ -0,0 +1,19 @@
+/**
+ * French translation for bootstrap-datetimepicker
+ * Nico Mollet <nico.mollet@gmail.com>
+ */
+;(function($){
+	$.fn.datetimepicker.dates['ar'] = {
+		days: ["الجمعة", "السبت", "الأحد", "الأثنين", "الثلاثاء", "الأربعاء", "الخميس", "الجمعة"],
+        daysShort:   [ "Fri","Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"],
+        daysMin:     ["ج", "س", "ح", "ت", "ث", "ر", "خ", "ج"],
+		months: ["يناير", "فبراير", "مارس", "ابريل", "مايو", "يونيو", "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"],
+		monthsShort: ["Jan", "Fev", "Mar", "Avr", "Mai", "Jui", "Jul", "Aou", "Sep", "Oct", "Nov", "Dec"],
+		today: "اليوم",
+		suffix: [],
+		meridiem: ["ص", "م"],
+		weekStart: 1,
+		format: "dd/mm/yyyy",
+        rtl: true
+	};
+}(jQuery));
